5ebacd49038f4ebfb7f07e736030dd6c380e9158
realest55 <babaran5434@gmail.com>
2025-08-01 12:05:33 +0300

    yaptığın işlemler

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as argon2 from 'argon2';
 import { SignUpDto } from './dto/signup-auth.dto';
 import { SignInDto } from './dto/signin-auth.dto';
 
@@ -18,9 +18,10 @@ export class AuthService {
   async signIn(signInDto: SignInDto): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(signInDto.email);
 
-    if (!user || !(await bcrypt.compare(signInDto.password, user.hash))) {
+    if (!user || !(await argon2.verify(user.hash, signInDto.password))) {
       throw new UnauthorizedException('Geçersiz kimlik bilgileri');
     }
+
     const payload = { sub: user._id, username: user.email, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
@@ -28,7 +29,7 @@ export class AuthService {
   }
 
   async signUp(signUpDto: SignUpDto) {
-    const hashedPassword = await bcrypt.hash(signUpDto.password, 10);
+    const hashedPassword = await argon2.hash(signUpDto.password);
 
     const { password, ...userData } = signUpDto;
     const newUser = await this.usersService.create({
